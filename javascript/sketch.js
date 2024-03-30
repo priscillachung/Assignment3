655c83161a5f5c8d1f9053251aedef7c3cfbd448
priscillachung <161750295+priscillachung@users.noreply.github.com>
2024-03-30 01:52:52 -0400

    Add files via upload

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -1,51 +1,75 @@
-// Gradient Background
-/*let c1,c2;
+let bgcolor;
+let fonts = [];
+let gradientColors = []; 
+let currentColorIndex = 0;
+let currentFontIndex = 0;
+
+function preload() {
+    fonts.push (loadFont('../assets/tbf.otf'));
+    fonts.push (loadFont('../assets/Imbue.ttf'));
+    fonts.push (loadFont('assets/RobotoMedium.ttf'));
+    fonts.push (loadFont('assets/RubikScribble.ttf'));
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  c1 = color(255);
-  c2 = color(63, 191, 191);
-  
- for(let y=0; y<height; y++){
-   n = map(y,0,height,0,1);
-    let newc = lerpColor(c1,c2,n);
-    stroke(newc);
-   line(0,y,width, y);
-  }
+    createCanvas(windowWidth, windowHeight);
+    bgcolor = color(255);
+    textSize(60);
+
+    gradientColors.push(color(200, 140, 244));
+    gradientColors.push(color(97, 218, 255));
+    gradientColors.push(color(115, 255, 194));
+    gradientColors.push(color(248, 255, 115));
+    gradientColors.push(color(255, 185, 115));
+    gradientColors.push(color(255, 115, 115));
 }
+
+function mouseWheel(event) {
+    if (event.delta > 0) {
+        currentColorIndex = (currentColorIndex + 1) % gradientColors.length;
+    } else {
+        currentColorIndex = (currentColorIndex - 1 + gradientColors.length) % gradientColors.length;
+    }
+
+    if (event.delta > 0) {
+        currentFontIndex = (currentFontIndex + 1) % fonts.length;
+    } else {
+        currentFontIndex = (currentFontIndex - 1 + fonts.length) % fonts.length;
+    }
+
+    return false;
+
+
+}
+
 function draw() {
-  background(220);
- console.log(c2);
-}*/
+    let nextColorIndex = (currentColorIndex + 1) % gradientColors.length;
+    let currentColor = gradientColors[currentColorIndex];
+    let nextColor = gradientColors[nextColorIndex];
 
-var bgcolor;
-let font1;
-
-function preload(){
-    //font1 = loadFont('assets/Imbue.ttf');
-    //font2 = loadFont('assets/RobotoMedium.ttf');
-    //font3 = loadFont('assets/RubikScribble.ttf');
+    setGradient(0, 0, windowWidth, windowHeight, currentColor, nextColor);
+    drawWords();
 }
 
-function setup () {
-  createCanvas(windowWidth, windowHeight);
-  bgcolor = color(255);
-  //textFont(font2);
-}
-
-function mouseWheel (event) {
-  bgcolor = random([color(200,140,244), color(97,218,255), color(115,255,194)]);
-  //textFont = random ([])
-}
-
-function draw () {
-  background(bgcolor);
-  drawWords(windowWidth * 0.5);
-}
-
-function drawWords(x){
+function drawWords() {
     fill(0);
-    text ('"Your choice of typeface is as important as what you do with it" - Bonnie Siegler', (windowWidth/2), (windowHeight*0.5));
-    textSize(50);
+    let quote = '"Your choice of typeface is as important as what you do with it" - Bonnie Siegler';
+    let wrappingWidth = windowWidth / 2;
+    textFont(fonts[currentFontIndex]);
     textAlign(CENTER, CENTER);
+    text(quote, windowWidth / 2, windowHeight / 2, wrappingWidth);
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function setGradient(x, y, w, h, c1, c2) {
+    noFill();
+    for (let i = y; i <= y + h; i++) {
+        let inter = map(i, y, y + h, 0, 1);
+        let c = lerpColor(c1, c2, inter);
+        stroke(c);
+        line(x, i, x + w, i);
+    }
 }
