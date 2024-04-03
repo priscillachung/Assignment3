81d0af260f82c3db048389d9f2ed6d8646aeaf51
priscillachung <161750295+priscillachung@users.noreply.github.com>
2024-04-03 15:38:32 -0400

    Add files via upload

--- javascript/sketch.js
+++ javascript/sketch.js
@@ -4,12 +4,26 @@ let gradientColors = [];
 let currentColorIndex = 0;
 let currentFontIndex = 0;
 let scrollCounter = 0;
+let rotationAngle = 0;
+let scrollUpCounter = 0;
+let scrollDownCounter = 0;
+
+let scrollBarHeight;
+let scrollBarWidth = 20;
+let scrollBarPosition = 0;
+
+let scrollingStarted = false;
 
 function preload() {
     fonts.push (loadFont('assets/tbf.otf'));
     fonts.push (loadFont('assets/Imbue.ttf'));
     fonts.push (loadFont('assets/RobotoMedium.ttf'));
     fonts.push (loadFont('assets/RubikScribble.ttf'));
+    fonts.push (loadFont('assets/Drunk.otf'));
+    fonts.push (loadFont('assets/Lostar.ttf'));
+    fonts.push (loadFont('assets/Muthiara.otf'));
+    fonts.push (loadFont('assets/Starborn.otf'));
+    fonts.push (loadFont('assets/Winter Memories.otf'));
 }
 
 function setup() {
@@ -19,44 +33,141 @@ function setup() {
 
     gradientColors.push(color(200, 140, 244));
     gradientColors.push(color(97, 218, 255));
+    gradientColors.push(color(115, 190, 255));
     gradientColors.push(color(115, 255, 194));
+    gradientColors.push(color(204, 255, 115));
     gradientColors.push(color(248, 255, 115));
     gradientColors.push(color(255, 185, 115));
     gradientColors.push(color(255, 115, 115));
+
+    scrollBarHeight = windowHeight;
 }
 
 function mouseWheel(event) {
 
-    scrollCounter++;
+                        // for every 10 scrolls, it changes background colour and typeface
+                        /*scrollCounter++;
 
-    if (scrollCounter % 10 === 0){
-        currentFontIndex = (currentFontIndex + 1) % fonts.length;
-        currentColorIndex = (currentColorIndex + 1) % gradientColors.length;
-    }
+                        if (scrollCounter % 5 === 0){
+                            // changes based on scroll direction
+                            currentFontIndex = (currentFontIndex + 1) % fonts.length;
+                            currentColorIndex = (currentColorIndex + 1) % gradientColors.length;
+                            rotationAngle += event.delta * 0.01;
+                        }*/
+                        
+                        //for every 1 scroll it changes background colour and typeface
+                        /* 
+                        if (event.delta > 0) {
+                            currentColorIndex = (currentColorIndex + 1) % gradientColors.length;
+                        } else {
+                            currentColorIndex = (currentColorIndex - 1 + gradientColors.length) % gradientColors.length;
+                        }
+
+                        if (event.delta > 0) {
+                            currentFontIndex = (currentFontIndex + 1) % fonts.length;
+                        } else {
+                            currentFontIndex = (currentFontIndex - 1 + fonts.length) % fonts.length;
+                        } */
+                        // Detect scroll direction
     
-    /* if (event.delta > 0) {
-        currentColorIndex = (currentColorIndex + 1) % gradientColors.length;
-    } else {
-        currentColorIndex = (currentColorIndex - 1 + gradientColors.length) % gradientColors.length;
+    if(!scrollingStarted){
+        scrollingStarted=true;
     }
 
     if (event.delta > 0) {
-        currentFontIndex = (currentFontIndex + 1) % fonts.length;
+        scrollUpCounter++;
+        scrollDownCounter = 0;
     } else {
-        currentFontIndex = (currentFontIndex - 1 + fonts.length) % fonts.length;
-    } */
+        scrollDownCounter++;
+        scrollUpCounter = 0;
+    }
 
+    // Change font every 5 scrolls up
+    if (scrollUpCounter >=5) {
+        currentFontIndex = (currentFontIndex + 1) % fonts.length;
+        scrollUpCounter = 0;
+    }
+
+    // Change background color every 5 scrolls down
+    if (scrollDownCounter >=5) {
+        currentColorIndex = (currentColorIndex + 1) % gradientColors.length;
+        scrollDownCounter = 0;
+    }
+
+    rotationAngle += event.delta * 0.01;
     return false;
-
-
 }
 
 function draw() {
+    // Calculating the next colour index
     let nextColorIndex = (currentColorIndex + 1) % gradientColors.length;
+    // getting the current and next colours
     let currentColor = gradientColors[currentColorIndex];
     let nextColor = gradientColors[nextColorIndex];
 
     setGradient(0, 0, windowWidth, windowHeight, currentColor, nextColor);
+
+    if (scrollingStarted) {
+        translate(50, height - 50);
+        rotate(rotationAngle);
+        rectMode(CENTER);
+        noFill();
+        stroke(255);
+        strokeWeight(2);
+        rect(50, 50, 800, 800);
+
+        translate(100, height - 100);
+        rotate(rotationAngle);
+        rectMode(CENTER);
+        noFill();
+        stroke(255);
+        strokeWeight(2);
+        rect(50, 50, 800, 800);
+
+        translate(500, height - 100);
+        rotate(rotationAngle);
+        rectMode(CENTER);
+        noFill();
+        stroke(255);
+        strokeWeight(2);
+        rect(50, 50, 800, 800);
+
+        translate(70, height - 5);
+        rotate(rotationAngle);
+        rectMode(CENTER);
+        noFill();
+        stroke(255);
+        strokeWeight(2);
+        rect(50, 50, 800, 800);
+
+        translate(5, height - 10);
+        rotate(rotationAngle);
+        rectMode(CENTER);
+        noFill();
+        stroke(255);
+        strokeWeight(2);
+        rect(50, 50, 800, 800);
+
+        translate(width - 50, height - 10);
+        rotate(rotationAngle);
+        rectMode(CENTER);
+        noFill();
+        stroke(255);
+        strokeWeight(2);
+        rect(50, 50, 800, 800);
+
+        translate(width - 100, height - 105);
+        rotate(rotationAngle);
+        rectMode(CENTER);
+        noFill();
+        stroke(255);
+        strokeWeight(2);
+        rect(50, 50, 800, 800);
+    }
+
+    resetMatrix();
+
+    noStroke();
     drawWords();
 }
 
@@ -74,6 +185,9 @@ function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
 
+// drawing the gradient background
+// draws a gradient between the current and next colours using linear interpolation
+// linear interpolation - calculates the value between 2 values 
 function setGradient(x, y, w, h, c1, c2) {
     noFill();
     for (let i = y; i <= y + h; i++) {
@@ -82,4 +196,4 @@ function setGradient(x, y, w, h, c1, c2) {
         stroke(c);
         line(x, i, x + w, i);
     }
-}
+} 
